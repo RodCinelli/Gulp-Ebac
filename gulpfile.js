2d55bec081cfd346eb4c15bc26e2ce743d06ca68
Rodrigo Cinelli <rodcinelli@gmail.com>
2024-03-31 05:40:58 -0300

    Aula 04

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+const gulp = require('gulp');
+
+function funcaoPadrao(callback) {
+    setTimeout(function() {
+        console.log("Executando via Gulp");
+        callback();   
+    }, 2000);
+}
+
+function dizOi(callback) {
+    setTimeout(function() {
+        console.log("Olá Gulp");
+        dizTchau();
+        callback();
+    }, 1000);
+}
+
+function dizTchau() {
+    console.log("Tchau Gulp");
+}
+
+exports.default = gulp.parallel(funcaoPadrao, dizOi);
+exports.dizOi = dizOi;
